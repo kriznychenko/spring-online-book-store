848ce2ac491fd6f7208a6746c9337593ac5ca3de
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2025-10-15 14:47:32 +0300

    fixed configuration

--- src/main/java/com/example/bookstore/SpringOnlineBookStoreApplication.java
+++ src/main/java/com/example/bookstore/SpringOnlineBookStoreApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringOnlineBookStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringOnlineBookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOnlineBookStoreApplication.class, args);
+    }
 
 }
