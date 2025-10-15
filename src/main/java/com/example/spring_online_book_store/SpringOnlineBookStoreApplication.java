e0ed9fa687e076f71316002cf55ee60cdf0f65ff
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2025-10-15 11:30:04 +0300

    initialized commit

---
+++ src/main/java/com/example/spring_online_book_store/SpringOnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_online_book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineBookStoreApplication.class, args);
+	}
+
+}
