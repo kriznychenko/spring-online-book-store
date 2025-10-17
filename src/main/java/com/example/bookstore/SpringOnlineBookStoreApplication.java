9c936d5c2dd38cc0e9efc1b49e4fa8e0aca398ae
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2025-10-17 12:44:15 +0300

    fixed checkstyle

--- src/main/java/com/example/bookstore/SpringOnlineBookStoreApplication.java
+++ src/main/java/com/example/bookstore/SpringOnlineBookStoreApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringOnlineBookStoreApplication {
